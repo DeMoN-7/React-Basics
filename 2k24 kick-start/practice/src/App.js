3678b3aedd7cf119ff9c95f243437bc244f99e04
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-01-10 02:22:41 +0530

    commit

---
+++ 2k24 kick-start/practice/src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <div className="header">
+          <div className="headerLeft">
+            <img src="https://marketplace.canva.com/EAFauoQSZtY/1/0/1600w/canva-brown-mascot-lion-free-logo-qJptouniZ0A.jpg" alt="Logo"/>
+          </div>
+          <div className="headerRight">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default App;
